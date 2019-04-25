c19bbdfef38e1d11c4e8248836df1764e4108515
Mildnoodles <adamt@vt.edu>
2019-04-25 15:39:37 -0400

    Got testing working

--- index.test.js
+++ index.test.js
@@ -3,9 +3,25 @@
 //import * as index from './js/index.js';
 const index = require('./js/index.js');
 //import userCount from './js/index.js';
-it('User Count Test', () => {
-    //feed.like(this);
-    //index.test("something");
-    //test('file:/Users/Adam/Documents/GitHub/AppForSWE/feedLogin.html');
+it('User Count Test 0', () => {
+    //Testing basic Method functionality 
+    expect(index.getCount()).toBe(0);
+});
+
+
+it('User Count Test 1', () => {
+    //Looking at the user count after a user has been created
+    index.createUser("a", "b", "c", "d", "e", "f");
     expect(index.getCount()).toBe(1);
 });
+
+it('User Count Test 2', () => {
+    //Looking at the user count after creating multiple users
+    index.createUser("a", "b", "c", "d", "e", "f");
+    index.createUser("a", "b", "c", "d", "e", "f");
+    index.createUser("a", "b", "c", "d", "e", "f");
+    index.createUser("a", "b", "c", "d", "e", "f");
+    expect(index.getCount()).toBe(5);
+});
+
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+// $('.message a').click(function(){
+//    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+// });
 
 var el1 = document.getElementById("login");
 if (el1) {
@@ -26,37 +26,39 @@ function login(form) {
 }
 
 function create(form) {
-   var firstName = document.getElementById('firstName').value;
-   var lastName = document.getElementById('lastName').value;
-   var displayName = document.getElementById('DisplayName').value;
-   var email = document.getElementById('email').value;
-   var password = document.getElementById('passwordC').value;
-   if (!validateEmail(email) || !validatePassword(password) || firstName == '' || lastName == '' || displayName == '') {
-      document.getElementById('alertC').innerHTML = 'Enter a valid informaiton';
+   var validEmail = false;
+   var validPassword = false;
+   var firstName;
+   if (document.getElementById('firstName')) {
+      firstName = document.getElementById('firstName').value;
+   }
+   var lastName;
+   if (document.getElementById('lastName')) {
+       lastName = document.getElementById('lastName').value;
+   }
+   var displayName;
+   if (document.getElementById('DisplayName')) {
+      var displayName = document.getElementById('DisplayName').value;
+   }   
+   var email;
+   if (document.getElementById('email')) {
+      email = document.getElementById('email').value;
+      validEmail = validateEmail(email);
+   }
+
+   var password;
+   if (document.getElementById('passwordC')) {
+      passowrd = document.getElementById('passwordC').value;
+      validPassword = validatePassword(password);
+   }
+      
+   if (!validEmail || !validPassword || firstName == '' || lastName == '' || displayName == '') {
+      if (document.getElementById('alertC')) {
+         document.getElementById('alertC').innerHTML = 'Enter a valid informaiton';
+      }
    }
    else {
-      var person = {
-         first: firstName,
-         last : lastName,
-         dispName : displayName,
-         pass     : password,
-         mail     : email,
-         id       : userCount,
-         fullName : function() {
-           return this.first + " " + this.last;
-         },
-         getid : function() {
-            return this.id;
-         },
-         getMail : function() {
-            return this.mail;
-         },
-         getDisp : function() {
-            return this.dispName;
-         }
-       };
-      users[userCount] = person;
-      userCount++;
+      users[userCount] = createUser(firstName, lastName, displayName, password, email, userCount);
       printUsers();
       window.open("file:/Users/Adam/Documents/GitHub/AppForSWE/feedLogin.html");
      
@@ -67,6 +69,14 @@ function getCount() {
    return userCount;
 }
 
+function setCount(x) {
+   userCount = x;
+}
+
+function reset() {
+   index.setCount(0);    
+}
+
 function printUsers() {
    for (i = 0; i < userCount; i++) {
       console.log(users[i].fullName());
@@ -74,13 +84,46 @@ function printUsers() {
    }
 }
 
+function createUser(firstName, lastName, displayName, password, email, count) {
+    var person = {
+         first: firstName,
+         last : lastName,
+         dispName : displayName,
+         pass     : password,
+         mail     : email,
+         id       : count,
+         fullName : function() {
+         return this.first + " " + this.last;
+         },
+         getid : function() {
+            return this.id;
+         },
+         getMail : function() {
+            return this.mail;
+         },
+         getDisp : function() {
+            return this.dispName;
+         }
+      };
+   userCount++;
+   return person;
+}
+
 function validateEmail(x) {
+   //Make sure it has an @
+   var amp = false;
    for (var i = 0; i < x.length; i++) {
       if(x.charAt(i) == "@") {
-         return true;
+         amp = true;
       }
-    }
-    return false;
+   }
+    //Make sure it is not a repeat email
+    for (i = 0; i < userCount; i++) {
+      if (users[i].email == x) {
+         return false;
+      }
+   }
+   return amp;
 }
 
 function validatePassword(x) {
@@ -98,9 +141,23 @@ function validateUser(x, y) {
    return false;   
 }
 
+
  var el3 = document.getElementById("info");
  if (el3) {
     el3.innerHTML = "";
  }
  
- 
+ module.exports = {
+   getCount: getCount,
+   setCount : setCount,
+   reset : reset,
+   users : users,
+   login : login,
+   create : create,
+   getCount : getCount,
+   printUsers : printUsers,
+   createUser : createUser,
+   validateEmail : validateEmail,
+   validatePassword : validatePassword,
+   validateUser : validateUser
+};
